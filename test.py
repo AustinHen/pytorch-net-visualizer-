7ba8c95fab705862e10ac6bfab35720759c7ac95
Austin Henlotter <ahenlotter@umass.edu>
2025-01-18 15:15:57 -0500

    finished rendering for both no data and with data nets

--- test.py
+++ test.py
@@ -1,1 +1,43 @@
-import visualizer.py
+import visualizer
+import matplotlib.pyplot as plt 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+
+def run_all_tests():
+    x = nn.Linear(28*28, 64).weight
+    print(x[0,0])
+    ##tester = visualizer.NNVisulizer()
+    ##tester.render_no_data()
+    test_2()
+
+def test_1():
+    print("running test 1") 
+    tester = visualizer.NNVisulizer()
+    tester.add_weight_layer(nn.Linear(10, 10).weight).add_weight_layer(nn.Linear(10, 5).weight)
+    tester.render()
+
+def test_2():
+    layers = []
+    tester = visualizer.NNVisulizer()
+    layers.append(nn.Linear(10, 10)) 
+    layers.append(nn.Linear(10, 5)) 
+    for layer in layers:
+        tester.add_weight_layer(layer.weight)
+    inp = torch.randn((10))
+    forward(layers, inp, tester)
+
+def forward(layers, inp, v):
+    v.add_node_layer(inp)
+    for layer in layers:
+        if layer == layers[-1]:
+            #usually something diff for last layer im just being lazy
+            inp = F.relu(layer(inp))
+            v.add_node_layer(inp)
+        else:
+            imp = F.relu(layer(inp))
+            v.add_node_layer(inp)
+    v.render(True)
+            
+if __name__ == "__main__":
+    run_all_tests()
--- visualizer.py
+++ visualizer.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt 
+import numpy as np
 import torch 
 
 class NNVisulizer:
-    POS_COLOR = "blue"
-    NEG_COLOR = "red"
+    POS_COLOR = "b"
+    NEG_COLOR = "r"
     MIN_WEIGHT = -1
     MAX_WEIGHT = 1
     MIN_NODE = -1
@@ -12,26 +13,127 @@ class NNVisulizer:
     def __init__(self):
         self.weights = []
         self.nodes = []
+        self.sizes = []
 
-    #TODO add args capablity to this function 
-    #adds weights via a list or single weight 
-    def add_weight_layer(self, weights):
-        #TODO add safegaurd for list of tensors
-        if type(weights) is list or type(weights) is torch.tensor:
-            self.weights.append(weights)
+    def add_weight_layer(self, *args):
+        for arg in args:
+            if type(arg) == "list":
+                self.weights.extends(arg)
+            else:
+                self.weights.append(arg)
+
+        return self
 
     def clear_weights(self):
         self.weights = []
-
+        return self
 
     def add_node_layer(self, nodes):
-        pass
+        self.nodes.append(nodes)
+        return self
  
     def clear_nodes(self):
         self.nodes = []
+        return self
 
-    def render_no_data(self):
-        pass
+    #TODO fix bug where all weights ingoing to a node are the same
+    def render(self, with_data=False):
+        if with_data:
+            assert len(self.nodes) == 1 + len(self.weights), "incorrect number of node layers"
 
-    def render_with_data(self):
-        pass
+        #sets up mpl 
+        fig, ax = plt.subplots(facecolor=".6")
+        
+        #gets size of nn / info to better display it
+        self.update_sizes()
+        center = self.get_center()
+
+        #gets offsets
+        offsets = []
+        for size in self.sizes:
+            offsets.append(center - size/2)
+
+        #used to determine if a node should be displayed
+        next_ingoing_sums = np.ones(self.sizes[0]).tolist()
+
+        #plots
+        for size_index, size in enumerate(self.sizes):
+            x_cord = size_index + 1
+            ingoing_sums = next_ingoing_sums
+            for y_cord in range(size):
+                offset = offsets[size_index]
+
+                #plots nodes
+                if ingoing_sums[y_cord] > 0:
+                    _color = [0, 0, 1]
+                    if with_data:
+                        #this prob all could be a one liner im just not seeing it
+                        node_val = float(self.nodes[0][y_cord])
+                        c_val = node_val
+                        c_val = -1 if c_val < -1 else c_val
+                        c_val = 1 if c_val > 1 else c_val
+                        #updates color
+                        primary_index = 2 if abs(node_val) == node_val else 0
+                        secondary_index = 0 if abs(node_val) == node_val else 2
+
+                        _color[secondary_index] = 0
+                        _color[primary_index] = abs(c_val)
+
+                    ax.plot(x_cord, y_cord+offset, 'o', color=_color, markeredgewidth=10, zorder=2)
+
+                #can skip rest if its the last layer
+                if(size_index+1 == len(self.sizes)):
+                    continue
+                
+                #plots outgoing weights 
+                next_ingoing_sums = np.zeros(self.sizes[size_index+1]).tolist()
+                next_offset = offsets[size_index+1]
+
+                for w_index in range(self.sizes[size_index+1]): 
+                    cur_weight_val = float(self.weights[size_index][w_index][y_cord])
+                    print("weight val")
+                    print(cur_weight_val)
+
+                    next_ingoing_sums[w_index] += abs(cur_weight_val)
+
+                    #draws line
+                    xs = [size_index+1, size_index+2]
+                    ys = [y_cord+offset, w_index+next_offset]
+
+                    _color = 'r' if cur_weight_val < 0 else 'b'
+                    _width = self.get_weight_line_width(cur_weight_val)
+
+                    if not self.weight_is_cutoff(cur_weight_val):
+                        plt.plot(xs, ys, color=_color, zorder=1, linewidth = _width)
+
+        #shows plot
+        plt.show()
+
+    def update_sizes(self):
+        self.sizes = []
+        for weight in self.weights:
+            try:
+                self.sizes.append(weight.size(dim=1))
+            except:
+                print("error -> weights likely incorrect format")
+                exit(1)
+        self.sizes.append(self.weights[-1].size(dim=0))
+
+    #returns center + 1 of largest node layer -> used to center the plot
+    def get_center(self):
+        largest = max(self.sizes)
+        return int(largest/2) + 1
+
+    #used to determine if a weight should be displayed
+    def weight_is_cutoff(self, w): 
+        return w == 0
+
+    #determines the line width given the weight
+    def get_weight_line_width(self, w):
+        return w*20
+
+    
+
+
+
+
